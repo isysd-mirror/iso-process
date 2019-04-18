485595c6a097e82c599887c456468021956fa56b
Ira Miller <isysd@isysd.guld>
2019-04-18 03:59:24 -0500

    sync and slow

--- test.js
+++ test.js
@@ -2,63 +2,43 @@
 import './process.js' //
 import { finishTest } from '../iso-test/index.js'
 
-async function setup () {
-  // set up test environment before loading module
-  if (typeof(window) === 'object' && window.localStorage) {
-    localStorage.clear()
-    if (window.sysenv) {
-      process.env.USER = window.sysenv.USER
-      process.env.HOME = window.sysenv.HOME
-      //process.env.PWD = window.sysenv.PWD
-    } else {
-      throw new Error('Unable to load environment vars.')
-    }
-    localStorage.setItem('env', JSON.stringify({
-      USER: process.env.USER,
-      HOME: process.env.HOME
-    }))
+if (typeof(window) === 'object' && window.localStorage) {
+  localStorage.clear()
+  if (window.sysenv) {
+    process.env.USER = window.sysenv.USER
+    process.env.HOME = window.sysenv.HOME
+    //process.env.PWD = window.sysenv.PWD
   } else {
-    // write USER, HOME, PWD to .env file for browser to load for testing
-/*    require('fs').writeFileSync('.env', `USER=${process.env.USER}
-HOME=${process.env.HOME}
-PWD=${process.env.PWD}
-`)*/
+    throw new Error('Unable to load environment vars.')
   }
+  localStorage.setItem('env', JSON.stringify({
+    USER: process.env.USER,
+    HOME: process.env.HOME
+  }))
 }
 
-async function run () {
-  // refresh environment from localStorage
-  // required after setup bootstrap, but not on reload
-  process.refreshEnv()
-  if (process.env && process.env.USER && process.env.USER !== 'undefined') finishTest('pass process.env.USER')
-  else finishTest(`fail process.env.USER: ${process.env.USER}`)
+// refresh environment from localStorage
+// required after setup bootstrap, but not on reload
+process.refreshEnv()
+if (process.env && process.env.USER && process.env.USER !== 'undefined') finishTest('pass process.env.USER')
+else finishTest(`fail process.env.USER: ${process.env.USER}`)
 
-  if (process.env.HOME === `/@${process.env.USER}`) finishTest('pass process.env.HOME')
-  else finishTest(`fail process.env.HOME: ${process.env.HOME}`)
+if (process.env.HOME === `/@${process.env.USER}`) finishTest('pass process.env.HOME')
+else finishTest(`fail process.env.HOME: ${process.env.HOME}`)
 
-  if (typeof(window) === 'object') {
-    if (process.title && process.title === 'browser') finishTest('pass title')
-    else finishTest(`fail process.title: ${process.title}`)
-  } else {
-    if (process.title && process.title === 'node') finishTest('pass title')
-    else finishTest(`fail process.title: ${process.title}`)
-  }
-
-  if (process.env.PWD && process.env.PWD.match(/\/process\/*$/)) finishTest('pass process.env.PWD')
-  else finishTest(`fail process.env.PWD: ${process.env.PWD}`)
-
-  if (process.cwd && process.cwd().match(/\/process\/*$/)) finishTest('pass process.cwd')
-  else finishTest(`fail process.cwd: ${process.cwd()}`)
-
-  // all passed! send kill to finishtest
-  finishTest('kill')
+if (typeof(window) === 'object') {
+  if (process.title && process.title === 'browser') finishTest('pass title')
+  else finishTest(`fail process.title: ${process.title}`)
+} else {
+  if (process.title && process.title === 'node') finishTest('pass title')
+  else finishTest(`fail process.title: ${process.title}`)
 }
 
-setup()
-  .catch(e => {
-    finishTest(e)
-  })
-  .then(run)
-  .catch(e => {
-    finishTest(e)
-  })
+if (process.env.PWD && process.env.PWD.match(/\/process\/*$/)) finishTest('pass process.env.PWD')
+else finishTest(`fail process.env.PWD: ${process.env.PWD}`)
+
+if (process.cwd && process.cwd().match(/\/process\/*$/)) finishTest('pass process.cwd')
+else finishTest(`fail process.cwd: ${process.cwd()}`)
+
+// all passed! send kill to finishtest
+finishTest('kill')
